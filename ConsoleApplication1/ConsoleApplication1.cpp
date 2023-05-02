28f40b8c55e4da3667af183073fd91af199ab28c
nikit <nikit@DESKTOP-5363RTP>
2023-05-02 17:22:46 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+class Car {
+public:
+	double X;
+	int P;
+	double V;
+};
+Car::Car() {
+	X = 0;
+	P = 1;
+	V = 2;
+}
+int main() {
+	using namespace std;
+	cout << "Test v3.01: OK.\n";
+	Car car;
+	cout << "X: " << car.X << endl;
+	cout << "P: " << car.P << endl;
+	cout << "V: " << car.V << endl;
+	return 0;
+};
